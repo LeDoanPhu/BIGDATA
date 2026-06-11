ccefb988a9c743708146dc64c6d9efdd1213cee4
FishCake9918 <khanhhoang9918@gmail.com>
2026-06-11 21:30:44 +0700

    hoàn thiện prod với consumer

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,4 +3,4 @@ HDFS_URL = "hdfs://100.109.78.104:9000"
 
 # Cấu hình đường dẫn Kafka Broker
 KAFKA_BROKER = "100.109.78.104:9092"
-KAFKA_TOPIC = "du_lieu_sinh_vien_realtime"
+KAFKA_TOPIC = "student_stream"
